e3ec01a588b717085117dd5ab09285fd29145b61
luisangeluis <luisangeluis@gmail.com>
2024-03-11 21:43:03 -0600

    Starting Repositories folder video 4 minute 9

---
+++ Orders/Orders.Frontend/Repositories/HttpResponseWrapper.cs
@@ -0,0 +1,18 @@
+﻿namespace Orders.Frontend.Repositories
+{
+    public class HttpResponseWrapper<T>
+    {
+        public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpActionResponseMessage)
+        {
+            Response = response;
+            Error = error;
+            HttpActionResponseMessage = httpActionResponseMessage;
+        }
+
+        public T? Response { get; }
+        public bool Error { get; }
+        public HttpResponseMessage HttpActionResponseMessage { get; }
+
+        public async Task<string?> 
+    }
+}
