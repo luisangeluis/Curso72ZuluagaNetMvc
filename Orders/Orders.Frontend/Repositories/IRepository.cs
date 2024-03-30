7d21ea093533413c63d1b96118a8caf0250d76f3
luisangeluis <luisangeluis@gmail.com>
2024-03-30 17:14:36 -0600

    Adding IRepository and Repository Video 4 minute 35

--- Orders/Orders.Frontend/Repositories/IRepository.cs
+++ Orders/Orders.Frontend/Repositories/IRepository.cs
@@ -4,8 +4,8 @@ namespace Orders.Frontend.Repositories
 {
     public interface IRepository
     {
-        Task<HttpResponseWrapper<T>> GetAsync(string url);
-        Task<HttpResponseWrapper<object>> PostAsync(string url, T model);
+        Task<HttpResponseWrapper<T>> GetAsync<T>(string url);
+        Task<HttpResponseWrapper<object>> PostAsync<T>(string url, T model);
         Task<HttpResponseWrapper<TActionResponse>> PostAsync<T,TActionResponse>(string url);
     }
 }
---
+++ Orders/Orders.Frontend/Repositories/Repository.cs
@@ -0,0 +1,46 @@
+﻿using NPOI.SS.Formula.Functions;
+using System.Text.Json;
+
+namespace Orders.Frontend.Repositories
+{
+    public class Repository : IRepository
+    {
+        private readonly HttpClient _httpClient;
+
+        //Esto para que sea capaz de Serializa o desserializar el objeto
+        private JsonSerializerOptions _jsonDefaultOptions =>new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+        };
+
+        public Repository(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<HttpResponseWrapper<T>> GetAsync<T>(string url)
+        {
+            var responseHttp = await _httpClient.GetAsync(url);
+
+            if (responseHttp.IsSuccessStatusCode)
+            {
+
+                var response = await UnserializeAnswer<T>(responseHttp);
+            }
+        }
+
+        private Task<Task> UnserializeAnswer<T>(HttpResponseMessage responseHttp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<HttpResponseWrapper<object>> PostAsync<T>(string url, T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<HttpResponseWrapper<TActionResponse>> PostAsync<T, TActionResponse>(string url)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
