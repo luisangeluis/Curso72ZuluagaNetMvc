25c056f2b585a3d80ff7a5f740eae57569bc10cc
luisangeluis <luisangeluis@gmail.com>
2024-04-03 22:35:00 -0600

    Video 5 minute 14, creating page countries index

--- Orders/Orders.Frontend/Repositories/IRepository.cs
+++ Orders/Orders.Frontend/Repositories/IRepository.cs
@@ -6,6 +6,6 @@ namespace Orders.Frontend.Repositories
     {
         Task<HttpResponseWrapper<T>> GetAsync<T>(string url);
         Task<HttpResponseWrapper<object>> PostAsync<T>(string url, T model);
-        Task<HttpResponseWrapper<TActionResponse>> PostAsync<T,TActionResponse>(string url);
+        Task<HttpResponseWrapper<TActionResponse>> PostAsync<T,TActionResponse>(string url, T model);
     }
 }
