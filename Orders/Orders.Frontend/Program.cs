51057618341ef5562dc1d6c885bd3514ef8a7d89
luisangeluis <luisangeluis@gmail.com>
2024-04-08 22:37:40 -0600

    Created request to show countries

--- Orders/Orders.Frontend/Program.cs
+++ Orders/Orders.Frontend/Program.cs
@@ -8,7 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7251") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7092/") });
 
 //Aqui se inyecto el repository para poder manejar las respuestas del backend
 builder.Services.AddScoped<IRepository,Repository>();
