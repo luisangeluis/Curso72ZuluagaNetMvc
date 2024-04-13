38c2c2cfb0ca331b10786c9b484afbb0e836ae1a
luisangeluis <luisangeluis@gmail.com>
2024-04-12 22:28:12 -0600

    Video 6 finished created a generic component

--- Orders/Orders.Frontend/Shared/GenericList.razor.cs
+++ Orders/Orders.Frontend/Shared/GenericList.razor.cs
@@ -4,13 +4,9 @@ namespace Orders.Frontend.Shared
 {
     public partial class GenericList<Titem>
     {
-        [Parameter]
-        public RenderFragment? Loading { get; set; }
-        [Parameter]
-        public RenderFragment? NoRecords { get; set; }
-        [EditorRequired,Parameter]
-        public RenderFragment? Body { get; set; } = null!;
-        [EditorRequired, Parameter]
-        public List<Titem> MyList { get; set; } = null!;
+        [Parameter] public RenderFragment? Loading { get; set; }
+        [Parameter] public RenderFragment? NoRecords { get; set; }
+        [EditorRequired,Parameter] public RenderFragment? Body { get; set; } = null!;
+        [EditorRequired, Parameter] public List<Titem> MyList { get; set; } = null!;
     }
 }
